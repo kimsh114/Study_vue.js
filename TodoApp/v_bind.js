bfe331cca4572fe7f2f991e0a9a4fd2d8dc2bb36
kimsh <108507848+kimsh114@users.noreply.github.com>
2023-02-12 16:41:15 +0900

    Delete v_bind.js

--- TodoApp/v_bind.js
+++
@@ -1,9 +0,0 @@
-// v-bind는 아이디, 클래스, 스타일 등의 HTML 속성값에 뷰 데이터 값을 연결할 때 사용하는 데이터 연결방식이다.
-
-new Vue ({
-    data:{
-        idA: 10,
-        classA:'container',
-        styleA: 'color : blue'
-    }
-}).$mount('#app');
